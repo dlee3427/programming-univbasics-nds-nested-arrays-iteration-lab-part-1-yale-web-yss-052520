063d8fba73c729d2d817f466b49e84d27f3fa82f
David Lee <david.lee.sl2634@yale.edu>
2020-05-15 01:17:10 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,9 @@ def find_even_values(src)
   counter = 0 
   while counter < src.length do
     inner_counter = 0 
-    while counter < array[counter].length do 
-      if array[counter] % 
+    while counter < src[counter].length do 
+      if array[counter] % 2 == 0 
+        p array[counter]
+      else 
     
 end
